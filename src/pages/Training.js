009acf6c7ba9524185816967cb7b966dec5189ed
DakshSuryavanshi <dakshsuryavanshi2003@gmail.com>
2023-04-22 13:09:02 +0530

    commit

--- src/pages/Training.js
+++ src/pages/Training.js
@@ -16,10 +16,8 @@ const training = () => {
         <section class="py-5 text-center container">
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
-                    <h1 class="fw-light">COURSES</h1>
-                    <p class="lead text-muted">Something short and leading about the collection below—its contents, the
-                        creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it
-                        entirely.</p>
+                    <h1 class="fw-light">SUMMER INTERNISHIP</h1>
+                    <p class="lead text-muted">Additionally , all the students will be groomed on Entrepreneural Skills, and Startup Formation. Interested Students or Student Group will be provided Mentorship for their startup IDEAS.</p>
                 </div>
             </div>
         </section>
