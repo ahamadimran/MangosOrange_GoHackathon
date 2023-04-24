86bba96d89aa57a3067ecad24e91f070527413fc
DakshSuryavanshi <dakshsuryavanshi2003@gmail.com>
2023-04-24 22:01:17 +0530

    commit

--- src/pages/Upcoming_Event.js
+++ src/pages/Upcoming_Event.js
@@ -1,136 +1,88 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Menu from './Menu';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
 
 const Upcoming_Event = () => {
-  return (
-    <Fragment>
-        <Menu />
+    return (
+        <Fragment>
+            <Menu />
 
-    <body className="body">
-          <div className="evebanner">
-            <img src="img/banner/upcoming_event.png" alt=""/>
-          </div>
+            <body className="body">
+                <div className="evebanner">
+                    <img src="img/banner/upcoming_event.png" alt="" />
+                </div>
 
-        <div class="album py-5 bg-light">
-            <div class="container"> 
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm">
-                          <Link to=""><img src="img/courses/AI&ML.jpg" alt="" height="100%" width="100%"/></Link>
-                            <div class="card-body">
-                                <h4>AI and Machine Learning</h4>
-                                {/* <p>
-                                    This course provides students with a comprehensive introduction to data science and
-                                     machine learning. Students will learn how to extract insights and knowledge from 
-                                     large data sets using popular data analysis and machine learning techniques.  
-                                </p>
-                               <Link to=""><a class="btn btn-primary" href="ai_ml.html" role="button">Buy Now</a></Link> &nbsp; &nbsp;
-                               <Link to="/ai_ml"><a class="btn btn-primary" href="ai-ml.html" role="button">Know More</a></Link> */}
+                <div class="album py-5 bg-light">
+                    <div class="container">
+                        <h2 class="text-center mx-auto text-white bg-info rounded p-2 m-3">MANTHAN</h2>
+                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 text-center">
+                            <div class="col-lg-4">
+                                <div class="card shadow-sm">
+                                    <Link to="/Gigabit"><img src="img\GIGABIT-01.png" alt="GIGABIT" height="200px" width="200px" /></Link>
+                                    <div class="card-body">
+                                        <h4>GIGABIT</h4>
+                                    </div>
+                                </div>
                             </div>
+                            <div class="col-lg-4">
+                                <div class="card shadow-sm">
+                                    <Link to="/Robosync"><img src="img\robosync-01-01.png" alt="Python" height="200px" width="200px" /></Link>
+                                    <div class="card-body">
+                                        <h4>ROBOSYNC</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="card shadow-sm">
+                                    <Link to="/Kurukshetra"><img src="img\kurukshetra-01.png" alt="Python" height="200px" width="200px" /></Link>
+                                    <div class="card-body">
+                                        <h4>KURUKSHETRA</h4>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm">
-                           <Link to="/full_stack_dev"><img src="img/courses/FullStack.jpg" alt="" height="100%" width="100%"/></Link>
-                            <div class="card-body">
-                                <h4>Full Stack Development</h4>
-                                {/* <p>
-                                    Students receive a thorough introduction to database and full stack development in 
-                                    this course. With the technical understanding of front-end and back-end programming, 
-                                    students will discover the nuances of data structures and algorithms.
-                                </p>
-                               <Link to=""><a class="btn btn-primary" href="full-stack-dev.html" role="button">Buy Now</a></Link> &nbsp; &nbsp;
-                               <Link to="/full_stack_dev"><a class="btn btn-primary" href="full-stack-dev.html" role="button">Know More</a></Link> */}
+                </div>
+
+                <div class="album py-5 bg-light">
+                    <div class="container">
+                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 text-center">
+                            <div class="col-lg-4">
+                                <div class="card shadow-sm">
+                                    <Link to="/Hackathon"><img src="img\hackathon-01.png" alt="HTML,CSS, JavaScript, BootStrap, Tailwind" height="200px" width="200px" /></Link>
+                                    <div class="card-body">
+                                        <h4>HACKATHON</h4>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm">
-                           <Link to=""><img src="img/courses/Aws_Devops.png" alt="" height="100%" width="100%"/></Link>
-                            <div class="card-body">
-                                <h4>Devops</h4>
-                                {/* <p>
-                                    Develop your knowledge of containers, deployment strategies, and immutable infrastructure. 
-                                    Create a portfolio for cloud computing containing labs, projects, and a capstone project. 
-                                    Discover AWS, DevOps, and more. Gain Practical Exposure to Projects & Laboratories. 
-                                </p>
-                               <Link to=""><a class="btn btn-primary" href="aws_devops.html"role="button">Buy Now</a></Link> &nbsp; &nbsp;
-                               <Link to="/aws_devops"><a class="btn btn-primary" href="aws_devops.html" role="button">Know More</a></Link> */}
+                            <div class="col-lg-4">
+                                <div class="card shadow-sm">
+                                    <Link to="/Strategem"><img src="img\STRATEGEM-01.png" alt="Python" height="200px" width="200px" /></Link>
+                                    <div class="card-body">
+                                        <h4>STRATEGEM</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="card shadow-sm">
+                                    <Link to="/Prayog"><img src="img\PRAYOG-01.png" alt="Python" height="200px" width="200px" /></Link>
+                                    <div class="card-body">
+                                        <h4>PRAYOG</h4>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="album py-5 bg-light">
-            <div class="container"> 
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col-lg-3">
-                        <div class="card shadow-sm">
-                        <Link to=""><img src="img/courses/cloud_computing.jpg" alt="" height="100%" width="100%"/></Link>
-                            <div class="card-body">
-                            <h4>Cloud Computing</h4>
-                                {/* <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.
-                                </p>
-                               <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Buy Now</a></Link> &nbsp; &nbsp;
-                               <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Know More</a></Link> */}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card shadow-sm">
-                        <Link to=""><img src="img/courses/python.jpg" alt="" height="100%" width="100%"/></Link>
-                            <div class="card-body">
-                            <h4>Python</h4>
-                                {/* <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.
-                                </p>
-                                <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Buy Now</a></Link> &nbsp; &nbsp;
-                                <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Know More</a></Link> */}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card shadow-sm">
-                        <Link to=""><img src="img/courses/java.png" alt="" height="100%" width="100%"/></Link>
-                            <div class="card-body">
-                            <h4>Java Programming</h4>
-                                {/* <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.
-                                </p>
-                                <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Buy Now</a></Link> &nbsp; &nbsp;
-                                <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Know More</a></Link> */}
-                            </div>
-                        </div>
-                    </div>
+            </body>
 
-                    <div class="col-lg-3">
-                        <div class="card shadow-sm">
-                        <Link to=""><img src="img/event/figma.png" alt="" height="100%" width="100%"/></Link>
-                            <div class="card-body">
-                            <h4>Figma Prototyping</h4>
-                                {/* <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.
-                                </p>
-                                <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Buy Now</a></Link> &nbsp; &nbsp;
-                                <Link to=""><a class="btn btn-primary" href="contact.html" role="button">Know More</a></Link> */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    
-    </body>
 
-    
-        <Footer />
-    </Fragment>
-  )
+            <Footer />
+        </Fragment>
+    )
 }
 
 export default Upcoming_Event
