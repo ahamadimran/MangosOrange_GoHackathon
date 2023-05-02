e08d15e9c640098f83d4e30572fd0f991e55642b
imranahmad369@gmail.com <ahamadimran369@gmail.com>
2023-05-02 14:09:19 +0530

    Update

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -20,19 +20,19 @@ const ContactForm = () => {
     <form id="contact_form" ref={form} onSubmit={sendEmail}>
       <div className="form-group">
         <label htmlFor="name"><strong>Full Name:</strong></label>
-        <input type="text" name="name" id="name" className="form-control" required />
+        <input type="text" name="name" id="name" className="form-control" placeholder='Enter Your Name' required />
       </div>
       <div className="form-group">
         <label htmlFor="email"><strong>Email:</strong></label>
-        <input type="email" name="email" id="email" className="form-control" required />
+        <input type="email" name="email" id="email" className="form-control" placeholder='Enter Your Mail' required />
       </div>
       <div className="form-group">
         <label htmlFor="number"><strong>Phone No:</strong></label>
-        <input type="number" name="number" id="number" className="form-control" required />
+        <input type="number" name="number" id="number" className="form-control" placeholder='Enter Your Phone No.' required />
       </div>
       <div className="form-group">
         <label htmlFor="message"><strong>Subject:</strong></label>
-        <textarea id="message" name="message" className="form-control" required />
+        <textarea id="message" name="message" className="form-control" placeholder='Kindly mention the course that you want to enroll' required />
       </div>
       <button className="btn btn-primary" type="submit" value="Submit">Submit</button>
     </form>
