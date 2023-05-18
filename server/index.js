5d9f1497d5b350f198b6b152f6b911e91c2563a0
imranahmad369@gmail.com <ahamadimran369@gmail.com>
2023-05-18 16:20:16 +0530

    Updates

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const sql = require("msnodesqlv8");
 
-const connectionString = "server=DESKTOP-E3PR7GQ\SQLEXPRESS;Database=gohackathon;Trusted_Connection=Yes;Driver={msnodesqlv8}";
-    
-const query = "SELECT * from dbo.coupons";
+const connectionString = "server=DESKTOP-E3PR7GQ\\SQLEXPRESS;Database=gohackathon;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 
-sql.query(connectionString, query, (err, recordSet) => {
-     console.log(err);
- });
+const query = "SELECT * FROM coupons";
+
+sql.query(connectionString, query, (err, rows) => {
+    console.log(rows);
+});
+
+// sql.connect(connectionString,function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     var request = new sql.Request();
+//     request.query('select * from coupons', function(err,recordSet){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log(recordSet);
+//         }
+//     });
+// })
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const dboperation = require('./dboperation');
 const cors = require('cors');
 
 const app = express();
@@ -7,13 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get('/message', ( req, res) => {
-      res.json({message: "Hello from Server"});
- });
-
-dboperation.getCoupons().then(res => {
-      console.log(res);
+    res.json({message: "Hello from Server"});
 });
 
- app.listen(8000, () => {
-      console.log('Server is running on port 8000');
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
 });
