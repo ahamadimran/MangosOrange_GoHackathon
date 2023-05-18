94c852faa794b29d2ffcae072722919144c6c33e
imranahmad369@gmail.com <ahamadimran369@gmail.com>
2023-05-18 17:57:53 +0530

    Server Update

--- server/index.js
+++ server/index.js
@@ -1,23 +1,17 @@
 const sql = require("msnodesqlv8");
 
-const connectionString = "server=DESKTOP-E3PR7GQ\\SQLEXPRESS;Database=gohackathon;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const connectionString="server=DESKTOP-E3PR7GQ\SQLEXPRESS;Database=gohackathon;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 
-const query = "SELECT * FROM coupons";
-
-sql.query(connectionString, query, (err, rows) => {
-    console.log(rows);
-});
-
-// sql.connect(connectionString,function(err){
-//     if(err){
-//         console.log(err);
-//     }
-//     var request = new sql.Request();
-//     request.query('select * from coupons', function(err,recordSet){
-//         if(err){
-//             console.log(err);
-//         }else{
-//             console.log(recordSet);
-//         }
-//     });
-// })
+sql.connect(connectionString,function(err){
+    if(err){
+        console.log(err);
+    }
+    var request = new sql.Request();
+    request.query('select * from coupons', function(err,recordSet){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(recordSet);
+        }
+    });
+})
