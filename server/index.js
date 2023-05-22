74e4daa95dcd450ae9b4fb9bb6392a99325057f6
imranahmad369@gmail.com <ahamadimran369@gmail.com>
2023-05-22 12:53:01 +0530

    Remove Tedious from Server Package

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 const sql = require("msnodesqlv8");
+var config={
+    server: "DESKTOP-E3PR7GQ\SQLEXPRESS",
+    database: "gohackathon",
+    driver: "msnodesqlv8",
+    options:{
+        trustedConnection: true
+    }
+};
 
-const connectionString="server=DESKTOP-E3PR7GQ\SQLEXPRESS;Database=gohackathon;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-
-sql.connect(connectionString,function(err){
+sql.ConnectionError(config,function(err){
     if(err){
-        console.log(err);
+        console.log("error");
     }
     var request = new sql.Request();
-    request.query('select * from coupons', function(err,recordSet){
+    request.query("select * from coupons", function(err, recordSet){
         if(err){
             console.log(err);
         }else{
-            res.send(recordSet);
+            console.log(recordSet);
         }
     });
-})
+});
