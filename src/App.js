011f782d7719464bfd4dbc7d99b0f524672175bf
imranahmad369@gmail.com <ahamadimran369@gmail.com>
2023-05-30 15:42:46 +0530

    Server removed

--- src/App.js
+++ src/App.js
@@ -62,11 +62,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/message")
-    .then((res) => res.json())
-    .then((data) => setMessage(data.message));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/message")
+  //   .then((res) => res.json())
+  //   .then((data) => setMessage(data.message));
+  // }, []);
 
   return (
     <div className="App">
